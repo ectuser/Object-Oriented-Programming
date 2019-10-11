a1106b0aeb9504540bbb297c1c1ca26375b003bb
ectuser <m.ectuser@gmail.com>
2019-10-11 14:34:31 +0700

    Some forms actions with buttons and so on.

--- SpaceStrategy/SpaceStrategy/Form1.cs
+++ SpaceStrategy/SpaceStrategy/Form1.cs
@@ -16,5 +16,16 @@ namespace SpaceStrategy
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputValue = textBox1.Text;
+            label1.Text = inputValue;
+        }
     }
 }
